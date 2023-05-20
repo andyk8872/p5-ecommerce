4612056dbad150b9036d88d55c0d7bd66a1e2be6
andyk8872 <andrewkennedy35@yahoo.ie>
2023-05-20 10:34:41 +0000

    updated context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,12 +1,28 @@
 # from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
+    """
+    To display the content in the shopping cart.
+    """
 
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if (total != 0) and (total < settings.FREE_DELIVERY_THRESHOLD):
         delivery = settings.STANDARD_DELIVERY_COST

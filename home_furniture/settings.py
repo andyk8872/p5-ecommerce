057f8a20b251a62cb31bab6def4bb5920509e37f
andyk8872 <andrewkennedy35@yahoo.ie>
2023-05-21 11:00:57 +0000

    refactor: changed allowed hosts

--- home_furniture/settings.py
+++ home_furniture/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-kgrej-$u#=vk8f2f48idj%%to6zrb8-dp#i4h4--uqfai^h4mq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-andyk8872-p5ecommerce-3vcxvwenw6z.ws-eu97.gitpod.io']
+ALLOWED_HOSTS = ['8000-andyk8872-p5ecommerce-3vcxvwenw6z.ws-eu97.gitpod.io', 'localhost']
 
 
 # Application definition

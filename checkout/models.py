381fd0b03ee4026f5e8b4f7d25a688c51c46fd6f
andyk8872 <andrewkennedy35@yahoo.ie>
2023-05-25 10:57:10 +0000

    feat: added checkout form, admn and signals.py

--- checkout/models.py
+++ checkout/models.py
@@ -12,26 +12,72 @@ class Order(models.Model):
     A Model to store information about a users
     order.
     """
-    order_number = models.CharField(max_length=32, null=False, editable=False)
-  
-    full_name = models.CharField(max_length=50, null=False, blank=False)
-    email = models.EmailField(max_length=254, null=False, blank=False)
-    phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
-    postcode = models.CharField(max_length=20, null=True, blank=True)
-    town_or_city = models.CharField(max_length=40, null=False, blank=False)
-    street_address = models.CharField(max_length=80, null=False, blank=False)
-    county = models.CharField(max_length=80, null=True, blank=True)
-    date = models.DateTimeField(auto_now_add=True)
+    order_number = models.CharField(
+        max_length=32,
+        null=False,
+        editable=False
+    )
+    full_name = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False
+    )
+    email = models.EmailField(
+        max_length=254,
+        null=False,
+        blank=False
+    )
+    phone_number = models.CharField(
+        max_length=20,
+        null=False,
+        blank=False
+    )
+    country = models.CharField(
+        max_length=40,
+        null=False,
+        blank=False
+    )
+    postcode = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True
+    )
+    town_or_city = models.CharField(
+        max_length=40,
+        null=False,
+        blank=False
+    )
+    street_address1 = models.CharField(
+        max_length=80,
+        null=False,
+        blank=False
+    )
+    street_address2 = models.CharField(
+        max_length=80,
+        null=True,
+        blank=True
+    )
+    county = models.CharField(
+        max_length=80,
+        null=True,
+        blank=True)
+    date = models.DateTimeField(
+        auto_now_add=True
+        )
     delivery_cost = models.DecimalField(
-        max_digits=10, decimal_places=2, null=False, default=0)
+        max_digits=6,
+        decimal_places=2,
+        null=False,
+        default=0
+           )
     order_total = models.DecimalField(
-        max_digits=10, decimal_places=2, null=False, default=0)
+        max_digits=10, decimal_places=2,
+        null=False,
+        default=0)
     grand_total = models.DecimalField(
-        max_digits=10, decimal_places=2, null=False, default=0)
-    original_bag = models.TextField(null=False, blank=False, default='')
-    stripe_pid = models.CharField(
-        max_length=254, null=False, blank=False, default='')
+        max_digits=10,
+        decimal_places=2,
+        null=False, default=0)
 
     def _generate_order_number(self):
         """
---
+++ checkout/signals.py
@@ -0,0 +1,21 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+
+@receiver(post_save, sender=OrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Update order total when lineitem is created
+    or updated
+    """
+    instance.order.update_total()
+
+
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_save(sender, instance, **kwargs):
+    """
+    Update order total on lineitem deleted
+    """
+    instance.order.update_total()

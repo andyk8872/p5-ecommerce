dfe42d980c50d796103f5077af50b70a901aad9b
andyk8872 <andrewkennedy35@yahoo.ie>
2023-05-26 09:06:40 +0000

    feat: added stripe elements, js and styles

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
 
@@ -12,8 +13,11 @@ def checkout(request):
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
-    context = {
+    context = {        
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MxlzYFfob9hCsSIbUaIjGoskXC9ag5laG7LTfk1gcdr345IopooQmiPxD4ktbfhHpACZhQarfNnu7hnfBFNOa5900LEy5EHdO',
+        # 'stripe_public_key': stripe_public_key,
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)

823571c7300f11f26a73b0baf04e28f87a3abf04
andyk8872 <andrewkennedy35@yahoo.ie>
2023-05-28 18:58:55 +0000

    revert changes from testing

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -114,7 +114,7 @@ class StripeWH_Handler:
                     status=500)
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | \
-                 SUCCESSFUL: Created order in webhook',
+                 SUCCESS: Created order in webhook',
             status=200)
 
     def handle_payment_intent_payment_failed(self, event):

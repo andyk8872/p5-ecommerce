ccacffa736582c15d8619cd1f8201bdedf3892ea
andyk8872 <andrewkennedy35@yahoo.ie>
2023-05-28 11:52:39 +0000

    feat: added checkout form data caching in payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -25,7 +25,7 @@ def cache_checkout_data(request):
         return HttpResponse(status=200)
     except Exception as e:
         messages.error(request, 'Sorry, your payment cannot be \
-            processed right now. Please try again later.')
+            processed. Please try again later.')
         return HttpResponse(content=e, status=400)
 
 
@@ -72,7 +72,8 @@ def checkout(request):
                     #         order_line_item.save()
                 except Product.DoesNotExist:
                     messages.error(request, (
-                        "One of the products in your bag wasn't found in our database. "
+                        "One of the products in your bag wasn't found in \
+                             our database. "
                         "Please call us for assistance!")
                     )
                     order.delete()
@@ -87,7 +88,8 @@ def checkout(request):
     else:
         bag = request.session.get('bag', {})
         if not bag:
-            messages.error(request, "There's nothing in your bag at the moment")
+            messages.error(request, "There's nothing in your bag \
+                at the moment")
             return redirect(reverse('products'))
 
         current_bag = bag_contents(request)

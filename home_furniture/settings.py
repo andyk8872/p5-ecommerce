0d8409b56d1cda03a1b89fa83c78c54d5165bc41
andyk8872 <andrewkennedy35@yahoo.ie>
2023-05-31 10:40:51 +0000

    refactor: changed local host

--- home_furniture/settings.py
+++ home_furniture/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-andyk8872-p5ecommerce-3vcxvwenw6z.ws-eu97.gitpod.io', 'localhost']
+ALLOWED_HOSTS = ['8000-andyk8872-p5ecommerce-3vcxvwenw6z.ws-eu98.gitpod.io', 'localhost']
 
 
 # Application definition

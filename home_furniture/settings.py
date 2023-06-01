3b9cd0e0835fe37d1360765044ad2221ac9ef316
andyk8872 <andrewkennedy35@yahoo.ie>
2023-06-01 23:40:17 +0000

    refactor: edit settings.py

--- home_furniture/settings.py
+++ home_furniture/settings.py
@@ -24,10 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ajk-furniture.herokuapp.com', '8000-andyk8872-p5ecommerce-3vcxvwenw6z.ws-eu98.gitpod.io', 'localhost']
 

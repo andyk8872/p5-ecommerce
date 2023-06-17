45b3bbce8b3e89b29ec2b4da001921108a66fa38
andyk8872 <andrewkennedy35@yahoo.ie>
2023-06-17 00:57:08 +0000

    refactor: removed commented out code

--- home_furniture/settings.py
+++ home_furniture/settings.py
@@ -28,19 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
 DEBUG = 'DEVELOPMENT' in os.environ
 
-# DEBUG = True
-
 ALLOWED_HOSTS = ['ajk-furniture.herokuapp.com', '8000-andyk8872-p5ecommerce-3vcxvwenw6z.ws-eu100.gitpod.io', 'localhost']
-# ALLOWED_HOSTS = ['ajk-furniture.herokuapp.com', 'localhost']
-
-# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
-EMAIL_HOST_USER = 'f37d95a84b107c'
-EMAIL_HOST_PASSWORD = 'MAILTRAP_PASSWORD'
-EMAIL_PORT = '2525'
 
 # Application definition
 
